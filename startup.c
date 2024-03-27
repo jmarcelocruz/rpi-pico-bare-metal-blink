0dc729af140eb955afe852ad1a8abab6d1f43eb5
José Marcelo Marques da Cruz <jmarcelocruz99@outlook.com>
2024-03-27 16:36:38 +0000

    Set up interrupt vector table

--- startup.c
+++ startup.c
@@ -16,3 +16,50 @@
 __attribute__((section(".boot"))) void _reset(void) {
     for (;;) { ; }
 }
+
+__attribute__((weak)) void NMI_Handler(void);
+__attribute__((weak)) void HardFault_Handler(void);
+__attribute__((weak)) void SVCall_Handler(void);
+__attribute__((weak)) void PendSV_Handler(void);
+__attribute__((weak)) void SysTick_Handler(void);
+
+__attribute__((weak)) void Timer0_IRQ_Handler(void);
+__attribute__((weak)) void Timer1_IRQ_Handler(void);
+__attribute__((weak)) void Timer2_IRQ_Handler(void);
+__attribute__((weak)) void Timer3_IRQ_Handler(void);
+__attribute__((weak)) void PWM_IRQ_Handler(void);
+__attribute__((weak)) void USBCTRL_Handler(void);
+__attribute__((weak)) void XIP_IRQ_Handler(void);
+__attribute__((weak)) void PIO0_0_IRQ_Handler(void);
+__attribute__((weak)) void PIO0_1_IRQ_Handler(void);
+__attribute__((weak)) void PIO1_0_IRQ_Handler(void);
+__attribute__((weak)) void PIO1_1_IRQ_Handler(void);
+__attribute__((weak)) void DMA0_IRQ_Handler(void);
+__attribute__((weak)) void DMA1_IRQ_Handler(void);
+__attribute__((weak)) void IO_Bank0_IRQ_Handler(void);
+__attribute__((weak)) void IO_QSPI_IRQ_Handler(void);
+__attribute__((weak)) void SIO_Proc0_IRQ_Handler(void);
+__attribute__((weak)) void SIO_Proc1_IRQ_Handler(void);
+__attribute__((weak)) void Clocks_IRQ_Handler(void);
+__attribute__((weak)) void SPI0_IRQ_Handler(void);
+__attribute__((weak)) void SPI1_IRQ_Handler(void);
+__attribute__((weak)) void UART0_IRQ_Handler(void);
+__attribute__((weak)) void UART1_IRQ_Handler(void);
+__attribute__((weak)) void ADC_FIFO_IRQ_Handler(void);
+__attribute__((weak)) void I2C0_IRQ_Handler(void);
+__attribute__((weak)) void I2C1_IRQ_Handler(void);
+__attribute__((weak)) void RTC_IRQ_Handler(void);
+
+extern void _estack(void);
+/* ARMv6-M Architecture Reference Manual, section B1.5.3 */
+__attribute__((section(".vectors"))) void (*vectors[])(void) = {
+    _estack, _reset, NMI_Handler, HardFault_Handler, 0, 0, 0, 0, 0, 0, 0,
+    SVCall_Handler, 0, 0, PendSV_Handler, SysTick_Handler, Timer0_IRQ_Handler,
+    Timer1_IRQ_Handler, Timer2_IRQ_Handler, Timer3_IRQ_Handler, PWM_IRQ_Handler,
+    USBCTRL_Handler, XIP_IRQ_Handler, PIO0_0_IRQ_Handler, PIO0_1_IRQ_Handler,
+    PIO1_0_IRQ_Handler, PIO1_1_IRQ_Handler, DMA0_IRQ_Handler, DMA1_IRQ_Handler,
+    IO_Bank0_IRQ_Handler, IO_QSPI_IRQ_Handler, SIO_Proc0_IRQ_Handler,
+    SIO_Proc1_IRQ_Handler, Clocks_IRQ_Handler, SPI0_IRQ_Handler, SPI1_IRQ_Handler,
+    UART0_IRQ_Handler, UART1_IRQ_Handler, ADC_FIFO_IRQ_Handler, I2C0_IRQ_Handler,
+    I2C1_IRQ_Handler, RTC_IRQ_Handler,
+};
