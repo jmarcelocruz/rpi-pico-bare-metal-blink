94a2ee818f431529561760e0907eb9c2fd4ce1cb
José Marcelo Marques da Cruz <jmarcelocruz99@outlook.com>
2024-04-15 23:44:13 +0100

    gpio: ensure subsystem resets complete before continuing

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -6,6 +6,10 @@
 
 void gpio_init(unsigned int gpio) {
     RESETS->reset &= ~(RESETS_IO_BANK0_BITS | RESETS_PADS_BANK0_BITS);
+    while ((RESETS->reset_done & RESETS_IO_BANK0_BITS) == 0 | (RESETS->reset_done & RESETS_PADS_BANK0_BITS) == 0) {
+        ;
+    }
+
     IO_BANK0->io[gpio].ctrl = GPIO_FUNC_SIO & IO_BANK0_GPIO_CTRL_FUNCSEL_BITS;
     PADS_BANK0->io[gpio] = ((1U << PADS_BANK0_GPIO_IE_LSB) & PADS_BANK0_GPIO_IE_BITS)
         | ((0U << PADS_BANK0_GPIO_OD_LSB) & PADS_BANK0_GPIO_OD_BITS);
